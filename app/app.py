4b981e5a67428408ac416ba321ec236dd58389db
sungjin-dev <tjdwlsl8888@gmail.com>
2026-07-08 17:02:45 +0900

    260708 app 연동 확인

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from member.route import member_bp, admin_bp
 from dashboard.blueprints.dashboard.routes import dashboard_bp
-
-
+from troubleshooting.trouble_routes import trouble_bp
 
 app = Flask(__name__)
 app.secret_key = "dw-aiot5th-20260622"
@@ -10,12 +9,11 @@ app.secret_key = "dw-aiot5th-20260622"
 app.register_blueprint(member_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(dashboard_bp)
+app.register_blueprint(trouble_bp)
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run(
