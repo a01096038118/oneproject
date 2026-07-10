d0488b5922ba5e78c56436e42f45f1ca086f19b1
riyoon <a0196038118@gmail.com>
2026-07-10 14:18:27 +0900

    user modify

--- blueprints/users/admin.py
+++ blueprints/users/admin.py
@@ -31,7 +31,7 @@ career_pattern = r'^\d+개월$'
 def adminSignUp_form():
     print('adminSignUp_confirm() CALLED')
 
-    return render_template('frontend/adminSignUp_form.html')
+    return render_template('adminSignUp_form.html')
 
 
 # 관리자 회원가입 양식
@@ -44,35 +44,35 @@ def adminSignUp_confirm():
     # id_pattern = r'^(?=.*[A-Za-z])[A-Za-z0-9]{4,20}$'
     mId = request.form['mId']
     if not re.match(id_pattern, mId):
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '아이디는 4자 이상 20자 이하로 입력해주세요.')
    
     # pw_pattern = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W_])[^\s]{8,20}$'
     mPw = request.form['mPw']
     if not re.match(pw_pattern, mPw):
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '비밀번호 특수문자, 영문, 숫자 포함해서 8자리 이상 입력해주세요.')
      
     # mail_pattern = r'^[\w.-]+@[\w.-]+\.[A-Za-z]{2,5}$'
     mMail = request.form['mMail']
     if not re.match(mail_pattern, mMail):
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '올바른 이메일 형식이 아닙니다.')
     
     # phone_pattern =r'^\d{10,11}$'
     mPhone = request.form['mPhone']
     if not re.match(phone_pattern, mPhone):
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '숫자만 입력해주세요.')
     
      # id 중복
     if mId in admins:
-        return render_template('frontend/adminSignUp_form.html',
+        return render_template('adminSignUp_form.html',
                                result = '중복된 ID 입니다. 다시 입력해주세요.')
     # email 중복
     for admin in admins.values():
         if admin ['mMail'] == mMail:
-            return render_template('frontend/adminSignUp_form.html',
+            return render_template('adminSignUp_form.html',
                                     result = '중복된 EMAIL입니다.')
         
     admin_keys = load_admin_keys()
@@ -98,7 +98,7 @@ def adminSignUp_confirm():
 
 
     return render_template(
-        'frontend/adminSignUp_result.html',
+        'adminSignUp_result.html',
         result = 'OK',
         admin_key = new_uuid)
 
@@ -106,7 +106,7 @@ def adminSignUp_confirm():
 # 관리자 로그인 화면 
 @admin_bp.route('/adminSignIn_form', methods = ['GET'])
 def adminSignIn_form():
-    return render_template('frontend/adminSignIn_form.html')
+    return render_template('adminSignIn_form.html')
 
 # 관리자 로그인 양식
 @admin_bp.route('/adminSignIn_confirm', methods = ['POST'])
@@ -122,14 +122,14 @@ def adminSignIn_confirm():
     
 
     if mId not in admins:
-        return render_template('frontend/adminSignIn_form.html',
+        return render_template('adminSignIn_form.html',
                                    result = 'ID가 존재하지 않습니다.')
     if admins [mId]['mPw'] != mPw:
-        return render_template('frontend/adminSignIn_form.html',
+        return render_template('adminSignIn_form.html',
                                result = '올바른 비밀번호가 아닙니다.') 
     #  키 존재 확인
     if admin_key not in admin_keys:
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '올바른 키번호가 아닙니다.')  
     
     save_admin_keys(admin_keys)
@@ -137,14 +137,14 @@ def adminSignIn_confirm():
     session['signedInAdminId'] = mId
     session['role'] = admins[mId]['role']
         
-    return render_template('/index.html', result = 'SIGNIN SUCCESS!!')
+    return render_template('index.html', result = 'SIGNIN SUCCESS!!')
 
 
 #ADMIN 클릭 시 관리자 로그인/회원가입 선택 페이지 리턴
 @admin_bp.route('/gateway', methods=['GET'])
 def admin_gateway():
 
-    return render_template('frontend/admin_gateway.html')
+    return render_template('admin_gateway.html')
 
 
 # 회원정보 리스트
@@ -154,7 +154,7 @@ def member_list():
     members = load_members()
 
     return render_template(
-        'frontend/member_list.html',
+        'member_list.html',
         members = members
         # members(html에 사용할 이름) = members(python 변수)
         # python 변수 members를 html에 사용할 이름 members에 전달한다.
@@ -171,7 +171,7 @@ def modify_form(mId):
     
     member = members[mId]
     
-    return render_template('frontend/modify_form.html',
+    return render_template('modify_form.html',
                            member = member)
 
 #  회원정보 수정 양식
@@ -182,7 +182,7 @@ def modify_confirm():
     admin_keys = load_admin_keys()
     
     if admin_key not in admin_keys:
-        return render_template('frontend/adminSignUp_result.html',
+        return render_template('adminSignUp_result.html',
                                result = '올바른 키번호가 아닙니다.') 
     
     members = load_members()
@@ -201,14 +201,14 @@ def modify_confirm():
 
         save_members(members)
 
-        return render_template('frontend/modify_result.html',
+        return render_template('modify_result.html',
                                 result = 'MODIFY SUCCESS!!')
 
 
 # member 삭제
 @admin_bp.route('/delete_form/<mId>')
 def delete_form(mId):
-    return render_template('frontend/delete_form.html', mId = mId)
+    return render_template('delete_form.html', mId = mId)
     
 @admin_bp.route('/delete_confirm', methods = ['POST'])
 def delete_confirm():
@@ -220,7 +220,7 @@ def delete_confirm():
 
     #  키 존재 확인
     if admin_key not in admin_keys:
-        return render_template('frontend/delete_form.html',
+        return render_template('delete_form.html',
                                mId = mId,
                                result = '올바른 키번호가 아닙니다.') 
     
@@ -231,6 +231,6 @@ def delete_confirm():
 
         save_members(members)
         
-    return redirect('/frontend/member_list')
+    return redirect('/member_list')
 
     
